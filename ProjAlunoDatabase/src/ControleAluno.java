58e33f5cbf2831ab880aa96a9fbf9a0858583031
william <162635122+oshirowilliamm@users.noreply.github.com>
2025-09-24 10:51:36 -0300

    Add files via upload

---
+++ ProjAlunoDatabase/src/ControleAluno.java
@@ -0,0 +1,21 @@
+public class ControleAluno {
+    public void Conectar (){
+        
+    }
+    
+    public void Cadastrar(){
+        
+    }
+    
+    public void Listar(){
+        
+    }
+    
+    public String Consultar(){
+        return "";
+    }
+    
+    public double CalcularMedia (int n1, int n2){
+        return 0;
+    }
+}
